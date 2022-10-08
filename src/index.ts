99ef6fcaa1c06cd0e411266d25556c2c4d63d004
Shashank Jha <shashank@eeposit.com>
2022-10-08 21:30:22 +0545

    refactored code and removed env file

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,4 @@ const app: Application = express();
 
 const PORT: number|string = process.env.PORT || 5000;
 
-app.listen(PORT, (): void => {
-    console.log("Server is running");
-});
+app.listen(PORT, (): void => console.log("Server is running"));
